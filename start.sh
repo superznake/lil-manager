820120c5e9cb86d360ff08d1101badf4a1870ab5
superznake <108449247+superznake@users.noreply.github.com>
2025-06-10 10:55:55 +0300

    .

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-screen -d -m -S pixelmon
-cd /home/mine/tst/PokehaanCraft2-2.10.0-ServerFiles/
-java -Djava.net.preferIPv4Stack=true -Xmx5G -Xms3G -jar forge-*.jar nogui
+screen -dmS pixelmon bash -c 'cd /home/mine/tst/PokehaanCraft2-2.10.0-ServerFiles/ && java -Djava.net.preferIPv4Stack=true -Xmx5G -Xms3G -jar forge-*.jar nogui'
